3cf89c4c01fb648385354a3acdae76d3e15ea6e8
Saurav-0403 <103096378+Saurav-0403@users.noreply.github.com>
2024-05-26 10:05:26 +0530

    Add files via upload

---
+++ Python/Activities/Activity_22_test.py
@@ -0,0 +1,36 @@
+
+'''
+Sum of two numbers
+Difference of two numbers
+Product of two numbers
+Quotient of two numbers
+'''
+import math
+import pytest
+
+def test_sum():
+    num1 = 10
+    num2 = 23
+    sum = num1 + num2
+    assert sum == 33
+
+def test_substract():
+    num1 = 26
+    num2 = 13
+    diff = num1 - num2
+    assert diff == 13
+
+@pytest.mark.activity
+def test_multiplication():
+    num1 = 10
+    num2 = 23
+    mul = num1 * num2
+    assert mul == 230
+
+
+@pytest.mark.activity
+def test_quotient():
+    num1 = 28
+    num2 = 3
+    quot = num1 // num2
+    assert quot == 9
---
+++ Python/Activities/Activity_23_test.py
@@ -0,0 +1,6 @@
+
+def test_sum(input_list):
+   sum = 0
+   for num in input_list:
+      sum += num
+   assert sum == 55
